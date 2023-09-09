6fa32bb745ff9fe82477f9ef79e703c23d5faf22
alonsitman <alonsitman@gmail.com>
2023-09-09 20:22:07 +0300

    improved scaling support for different window sizes

--- src/components/molecules/MoviePreview.tsx
+++ src/components/molecules/MoviePreview.tsx
@@ -1,4 +1,4 @@
-import { CSSProperties, FC } from "react";
+import { CSSProperties, FC, useEffect, useState } from "react";
 import MovieInfo from "../atoms/MovieInfo";
 import MovieLabel from "../atoms/MovieLabel";
 import MoviePoster from "../atoms/MoviePoster";
@@ -10,7 +10,13 @@ const moviePreviewStyle: CSSProperties = {
   flexDirection: "column",
   alignItems: "center",
   justifyContent: "center",
-  width: "100%"
+  width: "100%",
+};
+
+const descriptionContainerStyle: CSSProperties = {
+  maxHeight: "50px", // Adjust the max height as needed
+  overflowY: "auto",
+  padding: "10px",
 };
 
 export type MoviePreviewProps = {
@@ -19,8 +25,8 @@ export type MoviePreviewProps = {
   releaseYear: string;
   director: string;
   description: string;
-  isFavorite: boolean; // Add a prop for favorite status
-  onFavoriteToggle: () => void; // Add a prop for the toggle function
+  isFavorite: boolean;
+  onFavoriteToggle: () => void;
 };
 
 const MoviePreview: FC<MoviePreviewProps> = ({
@@ -32,34 +38,54 @@ const MoviePreview: FC<MoviePreviewProps> = ({
   isFavorite,
   onFavoriteToggle,
 }) => {
+  const [isSmallScreen, setIsSmallScreen] = useState(false);
+
+  useEffect(() => {
+    // Update the screen size state on window resize
+    const handleResize = () => {
+      setIsSmallScreen(window.innerWidth <= 768); // Adjust the breakpoint as needed
+    };
+
+    // Initial check for screen size
+    handleResize();
+
+    // Add event listener for window resize
+    window.addEventListener("resize", handleResize);
+
+    // Clean up the event listener when the component unmounts
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
   return (
-  <div style={moviePreviewStyle}>
-  <div style={{ position: "relative" }}>
-    <MoviePoster movieTitle={movieTitle} posterUrl={posterUrl} />
-    <button
-      onClick={onFavoriteToggle}
-      style={{
-        position: "absolute",
-        top: "10px",
-        right: "10px",
-        backgroundColor: "transparent",
-        border: "none",
-        cursor: "pointer",
-      }}
-    >
-      {isFavorite ? (
-        <span style={{ color: "red", fontSize: "24px" }}>&#10084;</span>
-      ) : (
-        <span style={{ color: "gray", fontSize: "24px" }}>&#10084;</span>
-      )}
-    </button>
-  </div>
-  <MovieLabel title={movieTitle} year={releaseYear} />
-  <MovieInfo label={"Director"} value={director} />
-  <MovieInfo label={"Description"} value={description} />
-</div>
-
+    <div style={moviePreviewStyle}>
+      <div style={{ position: "relative" }}>
+        <MoviePoster movieTitle={movieTitle} posterUrl={posterUrl} />
+        <button
+          onClick={onFavoriteToggle}
+          style={{
+            position: "absolute",
+            top: isSmallScreen ? "5px" : "10px",
+            right: isSmallScreen ? "5px" : "10px",
+            backgroundColor: "transparent",
+            border: "none",
+            cursor: "pointer",
+          }}
+        >
+          {isFavorite ? (
+            <span style={{ color: "red", fontSize: "24px" }}>&#10084;</span>
+          ) : (
+            <span style={{ color: "gray", fontSize: "24px" }}>&#10084;</span>
+          )}
+        </button>
+      </div>
+      <MovieLabel title={movieTitle} year={releaseYear} />
+      <MovieInfo label={"Director"} value={director} />
+      <div style={descriptionContainerStyle}>
+        <MovieInfo label={"Description"} value={description} />
+      </div>
+    </div>
   );
 };
 

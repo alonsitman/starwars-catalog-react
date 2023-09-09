4b4fa1afcd1d824c2711bae0c6dcf27353d5caf8
alonsitman <alonsitman@gmail.com>
2023-09-09 19:35:46 +0300

    added minimal functionality to fetch data from swapi

---
+++ src/services/StarWarsService.ts
@@ -0,0 +1,16 @@
+// src/services/starWarsService.ts
+export interface Movie {
+    episode_id: number;
+    title: string;
+    opening_crawl: string;
+  }
+  
+  export async function fetchMovies(): Promise<Movie[]> {
+    const response = await fetch('https://swapi.dev/api/films/');
+    if (!response.ok) {
+      throw new Error('Failed to fetch movies.');
+    }
+    const data = await response.json();
+    return data.results;
+  }
+  
